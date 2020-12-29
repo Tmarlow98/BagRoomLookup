4bfdcb82737916f8edf7bccd3abcd62409a13352
Tmarlow98 <32168250+Tmarlow98@users.noreply.github.com>
2020-12-29 12:08:49 -0500

    Update MainWindow.xaml.cs
    forgot to remove line of unfinished code

--- BagRoomApp/BagRoom/BagRoom/MainWindow.xaml.cs
+++ BagRoomApp/BagRoom/BagRoom/MainWindow.xaml.cs
@@ -15,11 +15,10 @@ namespace BagRoom
             InitializeComponent();
         }
 
-        private void Window_Loaded(string name, object sender, RoutedEventArgs e)
+        private void Window_Loadedobject sender, RoutedEventArgs e)
         {
             var query =
             from player in dataEntities.players
-            where player.first_name == name
             select new { player.id, player.first_name, player.last_name, player.bag_id };
 
             bagRoomGrid.ItemsSource = query.ToList();

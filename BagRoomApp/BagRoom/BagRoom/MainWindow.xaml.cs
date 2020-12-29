41dbe4d3cc7b0fe8013ce41893db7c9e88ba43f6
Tmarlow98 <32168250+Tmarlow98@users.noreply.github.com>
2020-12-29 12:06:17 -0500

    Add files via upload
    Created Windows Presentation Foundation Application
    Connected to Database
    On Button click prints players table to screen

---
+++ BagRoomApp/BagRoom/BagRoom/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Windows;
+
+namespace BagRoom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bagRoomEntities dataEntities = new bagRoomEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(string name, object sender, RoutedEventArgs e)
+        {
+            var query =
+            from player in dataEntities.players
+            where player.first_name == name
+            select new { player.id, player.first_name, player.last_name, player.bag_id };
+
+            bagRoomGrid.ItemsSource = query.ToList();
+        }
+    }
+}

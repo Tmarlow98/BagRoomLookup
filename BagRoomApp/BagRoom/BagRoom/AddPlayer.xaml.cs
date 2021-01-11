59d4a06fd3b20e64ae531968e75c18c528070f91
Tmarlow98 <32168250+Tmarlow98@users.noreply.github.com>
2021-01-11 13:36:49 -0500

    Add files via upload
    can now successfully lookup players based on last name or bag number
    Added unfinished files AppPlayer.xaml and AddPlayer.xaml.cs

---
+++ BagRoomApp/BagRoom/BagRoom/AddPlayer.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace BagRoom
+{
+    /// <summary>
+    /// Interaction logic for AddPlayer.xaml
+    /// </summary>
+    public partial class AddPlayer : Window
+    {
+        public AddPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void insertPlayer(object sender, RoutedEventArgs e)
+        {
+            string firstName = fName.Text;
+            string lastName = lName.Text;
+            int bagNumber;
+            int.TryParse(bagNum.Text, out bagNumber);
+
+            player newPlayer = new player();
+
+            newPlayer.first_name = firstName;
+            newPlayer.last_name = lastName;
+            newPlayer.bag_id = bagNumber;
+
+            
+                
+        }
+    }
+}
--- BagRoomApp/BagRoom/BagRoom/MainWindow.xaml.cs
+++ BagRoomApp/BagRoom/BagRoom/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.Entity.Core.Objects;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows;
 
 namespace BagRoom
@@ -15,13 +14,47 @@ namespace BagRoom
             InitializeComponent();
         }
 
-        private void Window_Loadedobject sender, RoutedEventArgs e)
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string stringInput = target.Text;
+            int numberInput = 0;
+
+            //Is input a number????
+            if (int.TryParse(stringInput, out numberInput))
+            {
+                var query =
+                    from player in dataEntities.players
+                    where player.bag_id == numberInput
+                    orderby player.id
+                    select new { player.id, player.first_name, player.last_name, player.bag_id };
+
+                bagRoomGrid.ItemsSource = query.ToList();
+            }
+            else 
+            {
+                    var query =
+                        from player in dataEntities.players
+                        where player.last_name == stringInput
+                        select new { player.id, player.first_name, player.last_name, player.bag_id };
+
+                    bagRoomGrid.ItemsSource = query.ToList();
+            }
+        }
+        
+
+        //prints table to main window
+        private void showTable(object sender, RoutedEventArgs e)
         {
             var query =
-            from player in dataEntities.players
-            select new { player.id, player.first_name, player.last_name, player.bag_id };
+               from player in dataEntities.players
+               select new { player.id, player.first_name, player.last_name, player.bag_id };
+               bagRoomGrid.ItemsSource = query.ToList();
+        }
 
-            bagRoomGrid.ItemsSource = query.ToList();
+        private void addPlayer(object sender, RoutedEventArgs e)
+        {
+            AddPlayer addPlayerWindow = new AddPlayer();
+            addPlayerWindow.Show();
         }
     }
 }
